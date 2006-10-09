ad7e0dbf530e5d7b74a41335248fc74d281ad7e6
meteo <meteo>
2006-10-09 14:50:54 +0000

    ajout de RealMeteoServer et TestMeteoServer qui implémentent l'interface MeteoServer

---
+++ meteo/java/pgm/meteo/engine/TestMeteoServer.java
@@ -0,0 +1,13 @@
+/**
+ * @author LE NY Clément
+ * @author CATRIC Jérôme
+ * @author YANG Yitian
+ * @author MEHEUT Emmanuel 
+ */
+package meteo.engine;
+
+public class TestMeteoServer implements MeteoServer{
+	public String getAMetarString(String code, long timeStamp){
+		return null;
+	}
+}
