e6226cd4c749ec65cfe466c50323023f8e31c96a
meteo <meteo>
2006-10-09 14:54:58 +0000

    ajout des 2 factory

---
+++ meteo/java/pgm/meteo/engine/MetarsFactory.java
@@ -0,0 +1,31 @@
+/**
+ * @author LE NY Clément
+ * @author CATRIC Jérôme
+ * @author YANG Yitian
+ * @author MEHEUT Emmanuel 
+ */
+package meteo.engine;
+import java.util.*;
+/**
+ * Classe MeteoEltFactory
+ * Construit des MeteosElt
+ */
+public class MetarsFactory {
+	/**
+	 * 
+	 * @param location
+	 * @param timeStamp
+	 * @return
+	 */
+	public Vector<Metar> getMetars(String location, long timeStamp){
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getPossibleLocations(){
+		return null;
+	}
+}
---
+++ meteo/java/pgm/meteo/engine/MeteoEltFactory.java
@@ -0,0 +1,17 @@
+/**
+ * @author LE NY Clément
+ * @author CATRIC Jérôme
+ * @author YANG Yitian
+ * @author MEHEUT Emmanuel 
+ */
+package meteo.engine;
+	import java.util.*;
+/**
+ * Classe MeteoEltFactory
+ * Construit des MeteosElt
+ */
+public class MeteoEltFactory {
+	public Hashtable getMeteoElts(Vector<Metar> metars){
+		return null;
+	}
+}
