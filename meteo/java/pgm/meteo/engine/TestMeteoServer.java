3597245de1a512073f41afa5d76b709f5a320343
meteo <meteo>
2006-10-25 14:02:30 +0000

    TestMeteoServer (non testé) test en cours de codage

--- meteo/java/pgm/meteo/engine/TestMeteoServer.java
+++ meteo/java/pgm/meteo/engine/TestMeteoServer.java
@@ -1,13 +1,48 @@
-/**
- * @author LE NY Clément
- * @author CATRIC Jérôme
- * @author YANG Yitian
- * @author MEHEUT Emmanuel 
- */
+
 package meteo.engine;
 
+import java.io.*; // lecture du fichier
+
+/**
+* @author LE NY Clément
+* @author CATRIC Jérôme
+* @author YANG Yitian
+* @author MEHEUT Emmanuel
+* Classe TestMeteoServer: elle est utilisée pour simuler le rôle
+* du serveur. Elle rendra une ligne Metar aléatoire à partir du fichier
+* test donné.
+*/
+
 public class TestMeteoServer extends MeteoServer{
+	
+	/**
+	 * @param code
+	 * @param timeStamp
+	 * @return Un metar sous forme textuelle.
+	 */
 	public String getAMetarString(String code, long timeStamp){
-		return null;
+		
+		String metar = "";
+		try {
+			// Ouverture du fichier
+			BufferedReader br = new BufferedReader(new FileReader("fichierTest"));
+			
+			//ligne à sélectionner  (entre 1 et 14)
+			int ligne = (int)(Math.random()*13+1);
+			
+			int i =0;
+			
+			while(i<ligne){
+				metar += br.readLine();
+				i++;
+			}
+			
+			
+			
+
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		}
+		return metar;
 	}
 }
---
+++ meteo/java/pgm/meteo/engineTest/TestMeteoServerTest.java
@@ -0,0 +1,17 @@
+package meteo.engineTest;
+
+import junit.framework.TestCase;
+import meteo.engine.TestMeteoServer;
+/**
+* @author LE NY Clément
+* @author CATRIC Jérôme
+* @author YANG Yitian
+* @author MEHEUT Emmanuel
+*/
+
+public class TestMeteoServerTest extends TestCase {
+	public void testGetAMetarString(){
+		//String test = TestMeteoServer.getAMetarString("",128);
+	}
+
+}
