f72bee2aef6942f3b15a5141073167435b65e9c9
meteo <meteo>
2006-11-08 18:35:38 +0000

    reste le blabla à faire

---
+++ meteo/java/pgm/meteo/engineTest/MetarsFactoryTest.java
@@ -0,0 +1,30 @@
+package meteo.engineTest;
+
+import java.util.Vector;
+
+import meteo.engine.Metar;
+import meteo.engine.MetarsFactory;
+import junit.framework.TestCase;
+
+/**
+ * Classe servant au test de la construction de metar pour une ville.
+ * 
+ * @author LE NY Clément
+ * @author CATRIC Jérôme
+ * @author YANG Yitian
+ * @author MEHEUT Emmanuel 
+ *
+ */
+public class MetarsFactoryTest extends TestCase {
+	
+	/**
+	 * Ce test vérifie que la fonction rend bien un 
+	 * vecteur de metar non null.
+	 */
+	public void testGetMetars(){
+		Vector<Metar> metars = null;
+		MetarsFactory Mfact = new MetarsFactory();
+		metars = Mfact.getMetars("Brest",1234);
+		assertNotNull(metars);
+	}
+}
