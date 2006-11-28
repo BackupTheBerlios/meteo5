2dcbf3f36cdb8e2aaf1513bef5cc8d9aea5b4794
meteo <meteo>
2006-11-28 13:42:26 +0000

    *** empty log message ***

--- meteo/java/pgm/AeroVille/AeroVille.java
+++ meteo/java/pgm/AeroVille/AeroVille.java
@@ -12,115 +12,229 @@ import InterfaceListener.GetListVilleListener;
 import InterfaceListener.ListVilleListener;
 import InterfaceListener.SelectedVilleListener;
 
-
-public class AeroVille implements Serializable, SelectedVilleListener, GetListVilleListener {
-	
+/**
+ * @author LE NY Clément
+ * @author CATRIC Jérôme
+ * @author YANG Yitian
+ * @author MEHEUT Emmanuel
+ * 
+ * Composant gérant les villes et aéroports.
+ * 
+ * Il émet les évènements : ListVille : contient la liste des villes pour
+ * lesquelles on peut avoir des informations météos. AeroVille : contient la
+ * liste des aéroports et leurs distances par rapport à la ville.
+ * 
+ * Il reçoit des évènements : GetListVille : contient une demande de la liste
+ * des villes. SelectedVille : contient l'information sur la ville permettant de
+ * trouver le/les code(s) des aéroprts.
+ * 
+ * Propriétés : aeroVilleFile : Nom du fichier où récupérer les informations sur
+ * les villes et aéroports aeroVilleFileSave : Nom du fichier de sauvegarde pour
+ * la persistance du composant.
+ */
+public class AeroVille implements Serializable, SelectedVilleListener,
+		GetListVilleListener {
 	private static final long serialVersionUID = 1l;
-		
-	private String aeroVilleFile = "";
-	
-	
+
+	/**
+	 * Constructeur vide nécessaire pour créer un composant.
+	 */
 	public AeroVille() {
-	
 	}
+
 	
 	
-	//------------------------------------------------
-	// Source de d'évènement AeroVille
 	
+	// ------------------------------------------------------------------------
+	// Source de d'évènement AeroVille : liste des codes d'aéroport + distance
+
+	/** Liste de composants écoutant l'évènement. */
 	private Vector<AeroVilleListener> aeroVilleListener = new Vector<AeroVilleListener>();
-	
+
+	/**
+	 * Ajoute un composant écoutant l'évènement.
+	 * 
+	 * @param l
+	 *            Composant écoutant l'évènement.
+	 */
 	public synchronized void addAeroVilleListener(AeroVilleListener l) {
 		this.aeroVilleListener.add(l);
 	}
-	
+
+	/**
+	 * Supprime un composant écoutant l'évènement.
+	 * 
+	 * @param l
+	 *            Composant à supprimer.
+	 */
 	public synchronized void removeAeroVilleListener(AeroVilleListener l) {
 		this.aeroVilleListener.remove(l);
 	}
-	
-	// Envoi des event AeroVille aux listener
-	private void chercherAeroports(String ville) {		
+
+	/**
+	 * Méthode chargée d'envoyer un évènement contenant les informations sur les
+	 * aéroports à tous les composants écoutant.
+	 * 
+	 * @param ville
+	 *            Ville dont on veut la liste des aéroports.
+	 */
+	private void chercherAeroports(String ville) {
+
+		// Création d'un objet pour l'évènement
 		AeroVilleEventObject aveo = new AeroVilleEventObject(this);
-		
+
+		// Récupération des informations voulues
 		LocationsFile locations = new LocationsFile(this.getAeroVilleFile());
 		aveo.setAeroports(locations.getLocValues(ville));
-		
+
+		// Envoi à tous les écoutant
 		synchronized (this) {
-			Vector<AeroVilleListener> l = (Vector<AeroVilleListener>)this.aeroVilleListener.clone();
-			
-			for(AeroVilleListener avl : l) {
+			Vector<AeroVilleListener> l = (Vector<AeroVilleListener>) this.aeroVilleListener
+					.clone();
+			for (AeroVilleListener avl : l) {
 				avl.handleChercherAeroports(aveo);
 			}
-	
 		}
+
 	}
+
 	
 	
-	//------------------------------------------------
-	// Source de d'évènement ListVille
 	
+	// ---------------------------------------------------------------
+	// Source de d'évènement ListVille : contient la liste des villes
+
+	/** Liste de composants écoutant l'évènement. */
 	private Vector<ListVilleListener> listVilleListener = new Vector<ListVilleListener>();
-	
+
+	/**
+	 * Ajoute un composant écoutant l'évènement.
+	 * 
+	 * @param l
+	 *            Composant écoutant l'évènement.
+	 */
 	public synchronized void addListVilleListener(ListVilleListener l) {
 		this.listVilleListener.add(l);
 	}
-	
+
+	/**
+	 * Supprime un composant écoutant l'évènement.
+	 * 
+	 * @param l
+	 *            Composant à supprimer.
+	 */
 	public synchronized void removeListVilleListener(ListVilleListener l) {
 		this.listVilleListener.remove(l);
 	}
-	
-	// Envoi des event ListVille aux listener
+
+	/**
+	 * Méthode chargée d'envoyer un évènement contenant les informations sur les
+	 * villes à tous les composants écoutant.
+	 */
 	private void getListVille() {
+
+		// Création d'un object pour l'évènement
 		ListVilleEventObject obj = new ListVilleEventObject(this);
-		
+
+		// Récupération des informations
 		LocationsFile locations = new LocationsFile(this.getAeroVilleFile());
 		obj.setVilles(locations.getLocations());
-		
+
+		// Envoi à tous les écoutants
 		synchronized (this) {
-			Vector<ListVilleListener> l = (Vector<ListVilleListener>)this.listVilleListener.clone();
-			
-			for(ListVilleListener avl : l) {
+			Vector<ListVilleListener> l = (Vector<ListVilleListener>) this.listVilleListener.clone();
+			for (ListVilleListener avl : l) {
 				avl.handleGetListVille(obj);
 			}
-	
 		}
 	}
+
 	
 	
-	//----------------------------------------------
-	// Ecouteur d'évènement SelectedVille
-	
-	// exe lors de l'arrivée d'un event
+	// ------------------------------------------------------------------------
+	// Ecouteur d'évènement SelectedVille : contient la ville pour la recherche
+	// des aéroports
+
+
+	/**
+	 * Méthode appelée lorsqu'un évènement SelectedVille est reçu.
+	 * @param e Objet contenant les informations sur la ville choisie.
+	 */
 	public void handleSelectedVille(SelectedVilleEventObject e) {
 		String ville = e.getVille();
-		
-		if(!ville.equals("")) {
+
+		if (!ville.equals("")) {
+			// Appel de la recherche de ville qui va générer un évènement
 			chercherAeroports(ville);
 		}
 	}
+
 	
-	//----------------------------------------------
-	// Ecouteur d'évènement GetListVille
 	
-	// exe lors de l'arrivée d'un event
+	// ----------------------------------------------
+	// Ecouteur d'évènement GetListVille : demande de la liste des villes
+
+	/**
+	 * Méthode azppelée lorsqu'un évènement GetListVille est reçu.
+	 * @param e Objet ne contenant pas d'information nécessaire.
+	 */
 	public void handleGetListVille(GetListVilleEventObject e) {
+		// Lance un évènement contenant la liste des villes
 		getListVille();
 	}
 	
 	
-	
-	//---------------------------------------------
-	// Accès aux propriétés
-	
+
+	// ---------------------------------------------
+	// Propriétés
+
+	/** Nom du fichier conteannt les informations sur les villes et aéroports. */
+	private String aeroVilleFile = "";
+
+	/**
+	 * Récupérer l'emplacement du fichier contenant les informations sur les
+	 * villes et aéroport.
+	 * 
+	 * @return L'emplacement du fichier contenant les informations sur les
+	 *         villes et aéroport.
+	 */
 	public String getAeroVilleFile() {
 		return this.aeroVilleFile;
 	}
-	
+
+	/**
+	 * Préciser l'emplacement du fichier contenant les informations sur les
+	 * villes et aéroport.
+	 * 
+	 * @param fileName
+	 *            L'emplacement du fichier contenant les informations sur les
+	 *            villes et aéroport.
+	 */
 	public void setAeroVilleFile(String fileName) {
 		this.aeroVilleFile = fileName;
 	}
-	
 
+	/** Nom du fichier servant à la persistance du composant. */
+	private String aeroVilleSaveFile = "";
 
-	
+	/**
+	 * Récupérer l'emplacement du fichier servant à la persistance du composant.
+	 * 
+	 * @return L'emplacement du fichier servant à la persistance du composant.
+	 */
+	public String getAeroVilleFileSave() {
+		return this.aeroVilleSaveFile;
+	}
+
+	/**
+	 * Préciser l'emplacement du fichier servant à la persistance du composant.
+	 * 
+	 * @param fileName
+	 *            L'emplacement du fichier servant à la persistance du
+	 *            composant.
+	 */
+	public void setAeroVilleFileSave(String fileName) {
+		this.aeroVilleSaveFile = fileName;
+	}
+
 }
