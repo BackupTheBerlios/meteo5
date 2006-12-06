769c4224cce42cfa96cebcbbff90a0afe3cca392
meteo <meteo>
2006-12-06 13:24:40 +0000

    *** empty log message ***

---
+++ meteo/java/pgm/Affichage/Affichage.java
@@ -0,0 +1,28 @@
+package Affichage;
+
+import java.io.Serializable;
+
+import InterfaceListener.TemperatureTraiteListener;
+
+
+/**
+ * @author LE NY Clément
+ * @author CATRIC Jérôme
+ * @author YANG Yitian
+ * @author MEHEUT Emmanuel
+ * 
+ * Composant traitant les informations pour créer un texte
+ * qui pourra être afficher par un client.
+ */
+public class Affichage implements Serializable { 
+	private static final long serialVersionUID = 1l;
+
+	
+	// ------------------------------------------------------------------------
+	// Récepteur des évènements contenant les informations météo :
+	// Temperature, Pressure, Weather, Wind, Visibility
+
+	
+
+
+}
