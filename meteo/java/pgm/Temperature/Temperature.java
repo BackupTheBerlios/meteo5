2b07f0c8e3cc8428f0ed7dae8956ec645d230142
meteo <meteo>
2006-12-07 13:20:35 +0000

    *** empty log message ***

--- meteo/java/pgm/Temperature/Temperature.java
+++ meteo/java/pgm/Temperature/Temperature.java
@@ -1,7 +1,10 @@
 package Temperature;
 
 import java.io.Serializable;
+import java.util.Vector;
 
+import EventObjects.TemperatureEventObject;
+import InterfaceListener.MetarListener;
 import InterfaceListener.TemperatureListener;
 
 /**
@@ -15,5 +18,31 @@ import InterfaceListener.TemperatureListener;
 public class Temperature implements TemperatureListener, Serializable{
 	static final long serialVersionUID =1;
 
+	//--------------------------------
+	// Source d'évènements Temperature
+	
+	/** liste des écouteurs d'évènements Metar */
+	private Vector<TemperatureListener> temperatureListeners = new Vector<TemperatureListener>();
+	
+	/** 
+	 * Ajout d'un écouteur.
+	 * @param l Ecouteur à ajouter à la liste des abbonnés.
+	 */
+	public synchronized void addMetarListener(TemperatureListener l) {
+		temperatureListeners.addElement(l);
+	}
+	
+	
+	
+	
+	
+	
+	
+
+	public void handleCalcul(TemperatureEventObject e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 	
 }
